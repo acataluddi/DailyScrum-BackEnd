4c5d90b2b1a9101e24e911b4cc3a08158369dc7c
Sanjo Joy <sanjo@qburst.com>
2018-10-09 10:05:54 +0530

    Implemented send E-mail functionality for feedbacks

--- src/main/java/com/qburst/Service/SendEmailService.java
+++ src/main/java/com/qburst/Service/SendEmailService.java
@@ -10,6 +10,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import com.qburst.Model.Feedback;
 import com.qburst.Model.ProjectMemberModel;
 import com.qburst.Model.UsersData;
 
@@ -54,8 +55,7 @@ public class SendEmailService {
 			if (member.getemail().equals(assignee.getEmail())) {
 				message.setSubject("Daily Scrum Updates");
 				message.setText("Hi " + name + "," + "\n\nYou have created '" + projectName
-						+ "' project under the Daily Scrum tool."
-						+ "\n\nThanks,\nDaily Scrum");
+						+ "' project under the Daily Scrum tool." + "\n\nThanks,\nDaily Scrum");
 				Transport.send(message);
 			} else {
 				message.setSubject("Allocation Update");
@@ -69,6 +69,51 @@ public class SendEmailService {
 
 			System.out.println("Done");
 
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void sendFeedbackNotificationEmail(Feedback feedback) {
+		InputStream input = null;
+		Properties property = new Properties();
+		try {
+			input = SendEmailService.class.getClassLoader().getResourceAsStream("mailAuth.properties");
+			property.load(input);
+			String host = property.getProperty("Mail_Host");
+			String socketport = property.getProperty("SocketFactory_PORT");
+			String socketclass = property.getProperty("SocketFactory_CLASS");
+			String authstatus = property.getProperty("SMTP_AUTH");
+			String port = property.getProperty("SMTP_PORT");
+			String fromID = property.getProperty("Email_ID");
+			String password = property.getProperty("Password");
+			String CC = property.getProperty("CC");
+
+			Properties props = new Properties();
+
+			props.put("mail.smtp.host", host);
+			props.put("mail.smtp.socketFactory.port", socketport);
+			props.put("mail.smtp.socketFactory.class", socketclass);
+			props.put("mail.smtp.auth", authstatus);
+			props.put("mail.smtp.port", port);
+
+			String to = property.getProperty("Admin_Mail_ID");
+
+			Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(fromID, password);
+				}
+			});
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(fromID));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(CC));
+			message.setSubject("Daily Scrum Updates");
+			message.setText("Hi,\n\nNew feedback has been submitted by " + feedback.getUserName() + ".\n\n" + "Name:\n"
+					+ feedback.getUserName() + "\nEmail:\n" + feedback.getUserEmail() + "\nFeedback:\n\""
+					+ feedback.getFeedbackDescription() + "\"\n\nThanks,\nDaily Scrum");
+			Transport.send(message);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
